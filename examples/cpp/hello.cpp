a1acbc870313cc2d1e2267c82118bd67ca7fb0ef
deshwal <priyendra@gmail.com>
2019-06-13 21:53:47 -0400

    Add devdocker script and an example devdocker config.

---
+++ examples/cpp/hello.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+// Steps to build/run the program:
+// $ devdocker exec g++ -o hello hello.cpp
+// $ devdocker exec ./hello
+
+int main() {
+  std::cout << "Hello world!\n";
+}
